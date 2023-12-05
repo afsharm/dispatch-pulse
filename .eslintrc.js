40e9ef951675b319cc3b20fa5385956d9aed90d4
afsharm <afshar.mohebbi@gmail.com>
2023-12-05 15:05:08 +0330

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es2020": true
+  },
+  "rules": {
+    "no-empty": "error",
+    "no-multiple-empty-lines": "warn",
+    "no-var": "error",
+    "prefer-const": "error"
+  }
+};
