a45e32ee5c88542082fb17bab1755af5f07920bc
afsharm <afshar.mohebbi@gmail.com>
2023-12-05 15:40:07 +0330

    eslint for test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
+  "plugins": ["jest"],
   "env": {
     "node": true,
-    "es2020": true
+    "es2020": true,
+    "jest/globals": true
   },
-  "extends": "eslint:recommended",
+  "extends": ["eslint:recommended", "plugin:jest/recommended"],
   "rules": {
     "no-multiple-empty-lines": "warn",
     "no-var": "error",
