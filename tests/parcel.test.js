3b47b3e27072cc43ea7a6d2fd8519336c9bc9ea2
afsharm <afshar.mohebbi@gmail.com>
2023-12-12 05:54:29 -0500

    test for get function

--- tests/parcel.test.js
+++ tests/parcel.test.js
@@ -4,17 +4,27 @@ const app = require('../app');
 const { setupTestDatabase } = require('./setupTestDatabase');
 
 beforeAll(async () => {
-    // Setup the test database before running any tests
-    await setupTestDatabase();
+  // Setup the test database before running any tests
+  await setupTestDatabase();
 });
 
 describe('Parcel API', () => {
-    it('should create a new parcel', async () => {
-        const res = await request(app)
-            .post('/parcel')
-            .send({ name: 'Sample Parcel' });
+  it('should create a new parcel', async () => {
+    const res = await request(app)
+      .post('/parcel')
+      .send({ name: 'Sample Parcel' });
 
-        expect(res.status).toBe(201);
-        expect(res.body.name).toBe('Sample Parcel');
-    });
+    expect(res.status).toBe(201);
+    expect(res.body.name).toBe('Sample Parcel');
+  });
+});
+
+describe('Parcel API Get', () => {
+  it('should retrieve parcel', async () => {
+    const res = await request(app)
+      .get('/parcel');
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([{ "id": 1, "name": "Sample Parcel" }]);
+  });
 });
